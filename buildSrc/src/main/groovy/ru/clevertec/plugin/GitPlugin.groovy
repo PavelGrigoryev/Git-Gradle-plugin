af6be321e76400177b0bd2af10a8fdfe184d9d6b
Grigoryev Pavel <Green-ono@yandex.ru>
2023-10-03 00:42:23 +0300

    add JGit

--- buildSrc/src/main/groovy/ru/clevertec/plugin/GitPlugin.groovy
+++ buildSrc/src/main/groovy/ru/clevertec/plugin/GitPlugin.groovy
@@ -1,9 +1,9 @@
 package ru.clevertec.plugin
 
+import org.eclipse.jgit.api.Git
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.internal.impldep.org.eclipse.jgit.api.Git
 
 class GitPlugin implements Plugin<Project> {
 
@@ -12,8 +12,7 @@ class GitPlugin implements Plugin<Project> {
         def map = [group: 'git', type: DefaultTask]
         project.task(map, 'gitTag') {
             doLast {
-                println "Hello World $project.projectDir"
-                Git git = Git.open(new File('https://github.com/PavelGrigoryev/Clever-Bank-Reactive-Remaster'))
+                Git git = Git.open(project.projectDir)
                 def list = git.tagList().call()
                 println list.toString()
             }
