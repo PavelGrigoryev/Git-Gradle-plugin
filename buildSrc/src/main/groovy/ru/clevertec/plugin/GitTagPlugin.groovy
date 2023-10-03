945308cd5e7ad91b4accd6f8e6371e1578c6c099
Grigoryev Pavel <Green-ono@yandex.ru>
2023-10-03 22:07:22 +0300

    add TagNotFoundException and remove JGIT dependency

--- buildSrc/src/main/groovy/ru/clevertec/plugin/GitTagPlugin.groovy
+++ buildSrc/src/main/groovy/ru/clevertec/plugin/GitTagPlugin.groovy
@@ -1,6 +1,5 @@
 package ru.clevertec.plugin
 
-import org.eclipse.jgit.api.Git
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -16,15 +15,40 @@ class GitTagPlugin implements Plugin<Project> {
                 println LOGO
             }
             doLast {
-                try (Git git = Git.open(project.projectDir)) {
-                    def tag = git.describe()
-                            .setTarget('HEAD')
-                            .setTags(true)
-                            .setMatch('*')
-                            .call()
-                    println tag
+                def file = project.projectDir
+                def tagVersion = getReferenceFromGit(file, 'git', 'describe', '--tags', '--abbrev=0')
+                println tagVersion
+                if (tagVersion.isEmpty()) {
+                    throw new TagNotFoundException('There is no tags in the project') // todo присвоить таг
+                }
+                def branchName = getReferenceFromGit(file, 'git', 'branch', '--show-current')
+                println branchName
+                switch (branchName) {
+                    case 'dev':
+                        println 'dev branch'
+                        break
+                    case 'qa':
+                        println 'qa branch'
+                        break
+                    default:
+                        println 'default branch'
+                        break
                 }
             }
+        }
+    }
+
+    private static def getReferenceFromGit(File file, String... commands) {
+        def process = new ProcessBuilder(commands)
+                .directory(file)
+                .start()
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            def temp = ''
+            def line = new StringBuilder()
+            while ((temp = reader.readLine()) != null) {
+                line.append(temp)
+            }
+            line.toString()
         }
     }
 
---
+++ buildSrc/src/main/groovy/ru/clevertec/plugin/TagNotFoundException.groovy
@@ -0,0 +1,9 @@
+package ru.clevertec.plugin
+
+class TagNotFoundException extends RuntimeException {
+
+    TagNotFoundException(String message) {
+        super(message)
+    }
+
+}
